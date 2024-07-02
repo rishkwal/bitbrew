9340e8d2efe793c89f219aeadb5b3b444de6d4b8
Rishabh <rishkwal@gmail.com>
2024-07-02 17:27:19 +0530

    chore: add @types/figlet as dev dependency

--- src/commands/brew.command.ts
+++ src/commands/brew.command.ts
@@ -1,11 +1,17 @@
 import { Command } from "commander";
 import DockerBitcionNetwork from "../DockerBitcoinNetwork";
+import figlet from 'figlet';
 
 export const BrewCommand = new Command()
     .name('brew')
     .description('Brew your own Bitcoin test network')
     .option('-n, --nodes <number>', 'Number of nodes to brew', '2')
     .action(async (options) => {
+        console.log(figlet.textSync('BitBrew',{
+            font: 'Puffy',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+          }));
         console.log('Brewing your Bitcoin network with', options.nodes, 'nodes...');
         const network = new DockerBitcionNetwork();
         network.initializeNodes(parseInt(options.nodes));
