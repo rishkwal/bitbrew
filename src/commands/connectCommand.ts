655047df4172be51020f063b483b7aaf25226d9d
Rishabh <rishkwal@gmail.com>
2024-07-14 14:00:50 +0530

    fix: fix connect -all option description

--- src/commands/connectCommand.ts
+++ src/commands/connectCommand.ts
@@ -4,7 +4,7 @@ import { connectAction } from '../actions/index.js'
 export const ConnectCommand = new Command()
     .name('connect')
     .description('Connect nodes in the Bitcoin network')
-    .option('-a, --all', 'Connect to all nodes in the network')
+    .option('-a, --all', 'Connect all nodes in the network in a linear fashion')
     .argument('[source-node]', 'Source node to connect')
     .argument('[target-node...]', 'Target nodes to connect')
     .action(async (sourceNode, targetNodes, options) => {
