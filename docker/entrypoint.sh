bc67adeb19f2e8cedd742e882aa84401894a2571
Rishabh <rishkwal@gmail.com>
2024-07-16 23:51:34 +0530

    fix(docker): use root user and directory changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-    echo $(whoami) 
-    cp /etc/bitcoin.conf "$BITCOIN_DATA/bitcoin.conf" \
-    && mkdir /root/.bitcoin \
-    && cp /etc/bitcoin.conf /root/.bitcoin/bitcoin.conf \
-    && chmod 600 ${BITCOIN_DATA}/bitcoin.conf
+    cp /etc/bitcoin.conf /root/.bitcoin/bitcoin.conf \
 
 exec "$@"
